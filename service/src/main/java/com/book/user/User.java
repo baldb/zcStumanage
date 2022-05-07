0c7b99c7fc1cef550e7b6de39f062d76d9b2293f
liny <704600358@qq.com>
2022-05-07 21:57:05 +0800

    测试前后端分离，交互测试

---
+++ service/src/main/java/com/book/user/User.java
@@ -0,0 +1,43 @@
+package com.book.user;
+
+/**
+ * @author 林逸
+ * cool boy
+ * 1.0
+ */
+public class User {
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
