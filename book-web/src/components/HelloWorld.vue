3efa16694f6a50744385323578828fa6aa2e66c1
cosart <xncai@sifei.info>
2022-05-07 21:14:18 +0800

    搭建前端框架

---
+++ book-web/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">前端构建</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
